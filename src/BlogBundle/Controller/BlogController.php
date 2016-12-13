c3b797a39b9a0a232d21f03969a76b0408201476
vyacheslav kovalyov <vyacheslav.kovalyov@simple-solution.biz>
2016-12-13 19:44:59 +0300

    Установлена пагинация но с багом  к переходу на несуществующие страницы prev next..

--- src/BlogBundle/Controller/BlogController.php
+++ src/BlogBundle/Controller/BlogController.php
@@ -1,52 +1,56 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: progi
- * Date: 05.07.2016
- * Time: 10:06
- */
 
 namespace BlogBundle\Controller;
 
 
 use BlogBundle\Entity\Blog;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class BlogController extends Controller
 {
 
-    public function homepageAction(){
+    public function homepageAction()
+    {
         return $this->render("::base.html.twig");
     }
 
     public function blogViewAction($id)
     {
-        $wtf=$this->getDoctrine();
-        $blogRepository=$wtf->getRepository("BlogBundle:Blog");
-        $blog=$blogRepository->find($id);
+        $wtf = $this->getDoctrine();
+        $blogRepository = $wtf->getRepository("BlogBundle:Blog");
+        $blog = $blogRepository->find($id);
 //        echo "<pre>";
 //        var_dump($blog);
 //        echo "</pre>";
-        return $this->render("BlogBundle:Blog:view.html.twig",[
-            'blog'=>$blog
+        return $this->render("BlogBundle:Blog:view.html.twig", [
+            'blog' => $blog
         ]);
     }
 
     /**
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function teaserAction()
+    public function blogAction(Request $request)
     {
-        $wtf=$this->getDoctrine();
-        $teaserRepository=$wtf->getRepository(Blog::class);
-        var_export(get_class($teaserRepository));
-        //$totalBlog=$teaserRepository->searchAllBlogCount();
-        //var_dump($totalBlog);
-//        $teasers=$teaserRepository->getBlog(['page'=>2]);
-        //var_dump($totalBlog);
-//        $teasers=$teaserRepository->findAll();
-        return $this->render("BlogBundle:Blog:teaser.html.twig",[
-            'teasers'=>null
+        $em = $this->getDoctrine();
+        $blogRepository = $em->getRepository("BlogBundle:Blog");
+        $countTotalBlog = $blogRepository->searchAllBlogCount();
+
+        $page = $request->query->get("page")
+            && $request->get("page") > 1 ? $request->query->get("page") : 1;
+
+        $blogsOnPage = $blogRepository->getBlog(['page' => $page]);
+        $pagination = [
+            "total" => $countTotalBlog,
+            "page" => $page,
+            "max_result" => 5,
+            "url" => "blog_teaser"
+        ];
+
+        return $this->render("BlogBundle:Blog:teaser.html.twig", [
+            'blogs' => $blogsOnPage,
+            'pagination' => $pagination
         ]);
     }
 }
